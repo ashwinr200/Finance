a4417fe2244450ed8886032d22abfc69fd40cf6e
ashwinr200 <45478410+ashwinr200@users.noreply.github.com>
2025-05-18 13:19:51 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,19 @@
-output "node_public_ip" {
-  value       = aws_instance.node.public_ip
-  description = "Public IP of node in environment ${var.env}"
-}
-
-output "node_private_ip" {
-  value       = aws_instance.node.private_ip
-  description = "Private IP of node in environment ${var.env}"
+output "master_private_ip" {
+  value       = aws_instance.master.private_ip
+  description = "Private IP of master instance"
 }
 
 output "master_public_ip" {
   value       = aws_instance.master.public_ip
-  description = "Public IP of master in environment ${var.env}"
+  description = "Public IP of master instance"
 }
 
-output "master_private_ip" {
-  value       = aws_instance.master.private_ip
-  description = "Private IP of master in environment ${var.env}"
+output "node_private_ip" {
+  value       = aws_instance.node.private_ip
+  description = "Private IP of node instance"
+}
+
+output "node_public_ip" {
+  value       = aws_instance.node.public_ip
+  description = "Public IP of node instance"
 }
