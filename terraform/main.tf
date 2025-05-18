47070f1fb65d3dea08f7b733f3905a1891392b7f
ashwinr200 <45478410+ashwinr200@users.noreply.github.com>
2025-05-18 17:47:43 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -77,20 +77,3 @@ resource "aws_instance" "node" {
               systemctl restart sshd
               EOF
 }
-
-# Outputs for Jenkins pipeline
-output "master_public_ip" {
-  value = aws_instance.master.public_ip
-}
-
-output "master_private_ip" {
-  value = aws_instance.master.private_ip
-}
-
-output "node_public_ip" {
-  value = aws_instance.node.public_ip
-}
-
-output "node_private_ip" {
-  value = aws_instance.node.private_ip
-}
