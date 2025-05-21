68ead323a51d432ae2ed663764d290c05fcf78f7
ashwinr200 <45478410+ashwinr200@users.noreply.github.com>
2025-05-21 16:40:17 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -30,3 +30,8 @@ curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/s
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt-get update -y
 sudo apt-get install -y terraform
+
+# Install Prometheus
+echo "Downloading Prometheus..."
+wget https://github.com/prometheus/prometheus/releases/download/v2.34.0/prometheus-2.34.0.linux-amd64.tar.gz
+tar zxvf prometheus-2.34.0.linux-amd64.tar.gz
