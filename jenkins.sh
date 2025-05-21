50fcda0ab0103a8efbadb2f03b2c26ea52726ebf
ashwinr200 <45478410+ashwinr200@users.noreply.github.com>
2025-05-21 23:16:09 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -50,6 +50,9 @@ wget https://github.com/prometheus/prometheus/releases/download/v2.34.0/promethe
 tar zxvf prometheus-2.34.0.linux-amd64.tar.gz
 cd /opt
 wget https://github.com/prometheus/node_exporter/releases/download/v1.6.1/node_exporter-1.6.1.linux-amd64.tar.gz
+
+
+
 tar -xvf node_exporter-1.6.1.linux-amd64.tar.gz
 mv node_exporter-1.6.1.linux-amd64 node_exporter
 
