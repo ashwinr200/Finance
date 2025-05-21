6bb4679e8e621999f475bd3c6da97dba33223614
ashwinr200 <45478410+ashwinr200@users.noreply.github.com>
2025-05-21 23:13:28 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -48,3 +48,8 @@ sudo apt-get install -y terraform
 echo "Downloading Prometheus..."
 wget https://github.com/prometheus/prometheus/releases/download/v2.34.0/prometheus-2.34.0.linux-amd64.tar.gz
 tar zxvf prometheus-2.34.0.linux-amd64.tar.gz
+cd /opt
+wget https://github.com/prometheus/node_exporter/releases/download/v1.6.1/node_exporter-1.6.1.linux-amd64.tar.gz
+tar -xvf node_exporter-1.6.1.linux-amd64.tar.gz
+mv node_exporter-1.6.1.linux-amd64 node_exporter
+
