d6fb55f1d15fd20a5f9b81cc8f55599f382e4d27
ashwinr200 <45478410+ashwinr200@users.noreply.github.com>
2025-05-21 16:34:40 +0530

    Create k8s master.sh

---
+++ k8s master.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+echo "=== Updating & Installing Kubernetes Master ==="
+sudo su
+apt update -y
+apt install -y wget
+
+# Download and execute master setup script
+wget https://raw.githubusercontent.com/akshu20791/Deployment-script/main/k8s-master.sh
+chmod +x k8s-master.sh
+./k8s-master.sh
+
+echo "=== Kubernetes Master Setup Complete ==="
+echo "Run the following on the master to get the join command:"
+echo ""
+echo "    kubeadm token create --print-join-command"
+echo ""
+echo "Append this to the output: --cri-socket unix:///var/run/cri-dockerd.sock"
