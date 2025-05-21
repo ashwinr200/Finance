42c5633ff30d0f50446ab384ad4cc16c71d9ba14
ashwinr200 <45478410+ashwinr200@users.noreply.github.com>
2025-05-21 16:39:21 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -4,6 +4,7 @@ sudo apt update -y
 sudo apt install openjdk-17-jdk -y
 sudo apt update -y
 sudo apt install maven -y
+sudo apt-get install -y git
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
@@ -16,3 +17,16 @@ cat /var/lib/jenkins/secrets/initialAdminPassword
 sudo usermod -aG docker jenkins
 #chmod 777 jenkins.sh
 #./jenkins.sh
+
+# Install Docker
+echo "Installing Docker..."
+sudo apt-get install -y docker.io
+echo "Adding Jenkins user to Docker group..."
+sudo usermod -aG docker jenkins
+
+# Install Terraform
+echo "Installing Terraform..."
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt-get update -y
+sudo apt-get install -y terraform
