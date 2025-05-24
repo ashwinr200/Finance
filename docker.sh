243a27550f96a64faa552855ee0b984d49935278
ashwinr200 <45478410+ashwinr200@users.noreply.github.com>
2025-05-24 18:22:46 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -3,7 +3,6 @@
 echo "Installing Docker..."
 sudo apt-get install -y docker.io
 # Add Docker's official GPG key:
-sudo apt-get update
 sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
