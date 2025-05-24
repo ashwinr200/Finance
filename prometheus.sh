9de5cc99c3fc4bd3787447fafdf5cb86d8d5e4ab
ashwinr200 <45478410+ashwinr200@users.noreply.github.com>
2025-05-24 23:35:43 +0530

    Update prometheus.sh

--- prometheus.sh
+++ prometheus.sh
@@ -1,4 +1,5 @@
 echo "Downloading Prometheus..."
+cd /opt
 wget https://github.com/prometheus/prometheus/releases/download/v2.34.0/prometheus-2.34.0.linux-amd64.tar.gz
 tar zxvf prometheus-2.34.0.linux-amd64.tar.gz
 mv prometheus-2.34.0.linux-amd64 prometheus
